509a53823a8ec9a7f87b3c9caf83a48b4c3b2180
Nicola Greco <notsecurity@gmail.com>
2013-03-22 16:37:46 +0100

    added handlebars + starting editor

--- build/build.js
+++ build/build.js
@@ -1446,41 +1446,7 @@ View.prototype.hide = function () {
 
 });
 require.register("boot/boot.js", function(exports, require, module){
-var Router = require('router');
 
-// r is handler for routing
-var r = {
-  homepage: function() {
-    console.log("YUHU WE ARE IN HOMEPAGE");
-  },
-  newpage: function() {
-    console.log("YUHU WE ARE IN NEWPAGE");
-  },
-  editor: function() {
-    console.log("YUHU WE ARE IN editor");
-  },
-  help: function() {
-    console.log("YUHU WE ARE IN help");
-  },
-  collection: function() {
-    console.log("YUHU WE ARE IN collection");
-  },
-  element_editor: function(type) {
-    console.log("YUHU WE ARE IN editor:"+type);
-  }
-};
-
-// router is the app route
-var router = new Router()
-  .route('#homepage', r.homepage)
-  .route('#new', r.newpage)
-  .route('#editor', r.editor)
-  .route("#help", r.help)
-  .route("#collection", r.collection)
-  .route('#editor/:type', r.element_editor);
-
-// starting history
-router.history.start();
 });
 require.alias("boot/boot.js", "happygui/deps/boot/boot.js");
 require.alias("boot/boot.js", "happygui/deps/boot/index.js");
