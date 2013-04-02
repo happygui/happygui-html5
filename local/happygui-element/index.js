4038b5eb41a56063fed5eafca7827f31430004d1
Nicola Greco <notsecurity@gmail.com>
2013-04-02 09:08:02 +0200

    introducing gesture and Element coords and test PreviewView

--- local/happygui-element/index.js
+++ local/happygui-element/index.js
@@ -2,6 +2,10 @@ function Element(options) {
   options = options || {};
   this.type = options.type;
   this.isDeletable = (options.isDeletable != undefined) ? options.isDeletable : true;
+  this.x = options.x || 0;
+  this.y = options.y || 0;
+  this.height = options.height || 30;
+  this.width = options.width || 30;
 }
 
 module.exports = Element;
