ae77cbe89a16d6f05285ddfd54e2e7d7ad72fcae
Nicola Greco <notsecurity@gmail.com>
2013-04-04 11:48:34 +0200

    colorpicker first implementation

--- build/build.js
+++ build/build.js
@@ -2519,8 +2519,10 @@ function Element(options) {
   options = options || {};
   this.type = options.type;
   this.isDeletable = (options.isDeletable != undefined) ? options.isDeletable : true;
-  this.x = options.x;
-  this.y = options.y;
+  this.x = options.x || 0;
+  this.y = options.y || 0;
+  this.height = options.height || 30;
+  this.width = options.width || 30;
 }
 
 module.exports = Element;
