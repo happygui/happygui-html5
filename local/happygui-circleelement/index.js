2e1d777669e3ba8752821c34f8303f8d0208a94e
Nicola Greco <notsecurity@gmail.com>
2013-04-17 12:38:47 +0200

    Radius is now considered in editor

--- local/happygui-circleelement/index.js
+++ local/happygui-circleelement/index.js
@@ -5,7 +5,7 @@ var CircleElement = function(options) {
   options = options || {};
 
   this.hasRadius = true;
-  this.radius = options.radius || 60;
+  this.r = options.r || 60;
 };
 CircleElement.prototype = Object.create(ShapeElement.prototype);
 CircleElement.prototype.constructor = CircleElement;
@@ -18,7 +18,8 @@ CircleElement.prototype.redraw = function () {
   this.drawing.attr({
     stroke: this.borderColor,
     fill: this.backgroundColor,
-    "stroke-width": this.borderThickness
+    "stroke-width": this.borderThickness,
+    r: this.r
   });
 
   return this;
@@ -28,7 +29,7 @@ CircleElement.prototype.draw = function (paper, callback) {
   var self = this;
 
   this.drawing = paper
-    .circle(self.x, self.y, self.radius)
+    .circle(self.x, self.y, self.r)
     .attr({
       stroke: self.borderColor,
       fill: self.backgroundColor,
@@ -40,10 +41,10 @@ CircleElement.prototype.draw = function (paper, callback) {
     .drag(
       function (dx, dy) {
         self.borderThickness = parseInt(self.borderThickness);
-        self.radius = parseInt(self.radius);
+        self.r = parseInt(self.r);
         this.attr({
-          cx: Math.min(Math.max(self.x + dx, self.radius + self.borderThickness), 480-(self.radius + self.borderThickness)),
-          cy: Math.min(Math.max(self.y + dy, self.radius + self.borderThickness), 600-(self.radius + self.borderThickness))
+          cx: Math.min(Math.max(self.x + dx, self.r + self.borderThickness), 480-(self.r + self.borderThickness)),
+          cy: Math.min(Math.max(self.y + dy, self.r + self.borderThickness), 600-(self.r + self.borderThickness))
         });
       },
       function () {
